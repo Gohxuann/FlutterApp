e79362534b82c54e13bc039ecb850b96d296e8f8
Gohxuann <122100966+Gohxuann@users.noreply.github.com>
2024-12-26 17:40:16 +0800

    Update myconfig.dart

--- mymembership_app/lib/myconfig.dart
+++ mymembership_app/lib/myconfig.dart
@@ -1,7 +1,8 @@
 class MyConfig {
   static const String servername1 = "http://172.20.10.5"; //u
-  static const String servername2 = "http://192.168.112.28"; //ab
+  static const String servername5 = "http://192.168.112.28"; //ab
 
   static const String servername3 = "http://10.19.58.122"; //ac
   static const String servername4 = "http://192.168.0.159"; //290
+  static const String servername2 = "http://10.19.18.45"; //291
 }
